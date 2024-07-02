37e027610e92bb6058220439a4d544f153a5c1c0
DAVI <davishillebrand8@gmail.com>
2024-07-02 11:25:12 -0300

    listadetarefa

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+function Header(props) {
+    return (
+        <h1>Olá, {props.name}!</h1>
+    );
+}
+
+export default Header;
+
+
+// import '../style.css';
+
+// function Header() {
+//     let logo = "https://via.placeholder.com/100";
+
+//     return (
+//         <header>
+//             <img src={logo} alt='logo' />
+//             <nav>
+//                 <a href="https://react.dev/" target='_blank' rel='noreferrer'>Home</a>
+//                 <a href="https://react.dev/learn" target='_blank' rel='noreferrer'>Perfil</a>
+//                 <a href="https://react.dev/community" target='_blank' rel='noreferrer'>Notificações</a>
+//             </nav>
+//         </header>
+//     );
+// }
+
+// export default Header;
